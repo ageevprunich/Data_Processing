731160fc7d40092e306dd91258cd5dfd07fc5cca
Serhii <99029543+ageevprunich@users.noreply.github.com>
2023-01-30 13:13:02 +0200

    Changes

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,6 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-        System.out.println("Hello other worlds!");
+        System.out.println("Hello other!");
     }
 }
