41304fe4e4c1293f64977c6ad9ae7ad8d42c757a
Serhii <99029543+ageevprunich@users.noreply.github.com>
2023-02-02 00:40:23 +0200

    Connect Angular

--- lab1/LW1B/src/main/java/Servlets/Servlet.java
+++ lab1/LW1B/src/main/java/Servlets/Servlet.java
@@ -1,2 +1,19 @@
-package Servlets;public class Servlet {
+package Servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servlet")
+public class Servlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Hello World !");
+    }
 }
