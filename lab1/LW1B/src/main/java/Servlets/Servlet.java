386756e7b058821a62aeb06d5da33b3d6feab825
Serhii <99029543+ageevprunich@users.noreply.github.com>
2023-02-02 20:16:41 +0200

    Main Part Finished

--- lab1/LW1B/src/main/java/Servlets/Servlet.java
+++ lab1/LW1B/src/main/java/Servlets/Servlet.java
@@ -1,5 +1,7 @@
 package Servlets;
 
+import Entity.Entity;
+import com.google.gson.Gson;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,13 +9,25 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 @WebServlet("/servlet")
 public class Servlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        Entity entity = new Entity("ID Card", 14, "You will get your ID Card after 14 days");
+
+        ArrayList<Entity> data =new ArrayList<Entity>();
+        data.add(entity);
+
+        String answer = new Gson().toJson(data);
+
         PrintWriter out = response.getWriter();
-        out.println("Hello World !");
+        response.setContentType("aplication/json");
+        response.setCharacterEncoding("UTF-8");
+        out.println(answer);
+        out.flush();
     }
 }
